ebfb9084e1bd4907db272fa00f56d78cd4cc681a
Adham-Abdelmaksoud <adham.5050@hotmail.com>
2023-10-23 16:29:15 +0300

    Implemented getMin() function

--- Main.cpp
+++ Main.cpp
@@ -11,9 +11,18 @@ int getSum(vector<int>& vectorToBeSummed){
     return sum;
 }
 
+int getMin(vector<int>& vectorToBeMinimized){
+    int minNum = 0;
+    for(int i=0 ; i<vectorToBeMinimized.size() ; i++){
+        minNum = min(minNum, vectorToBeMinimized[i]);
+    }
+    return minNum;
+}
+
 int main(){
     vector<int> vectorToBeSummed{2, 6, 17, 7, 8, 21, 3, 9, 1, 13};
     cout<<"Sum of Vector Elements = "<<getSum(vectorToBeSummed)<<'\n';
+    cout<<"Minimum of Vector Elements = "<<getMin(vectorToBeSummed)<<'\n';
 
     return 0;
 }

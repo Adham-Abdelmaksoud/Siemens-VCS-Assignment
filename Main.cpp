d8d841edde72d6efcabcbb6698a4ca23c94a0762
Adham-Abdelmaksoud <adham.5050@hotmail.com>
2023-10-23 16:26:00 +0300

    Implemented getAverage() function

--- Main.cpp
+++ Main.cpp
@@ -11,9 +11,18 @@ int getSum(vector<int>& vectorToBeSummed){
     return sum;
 }
 
+float getAverage(vector<int>& vectorToBeAveraged){
+    float sum = 0;
+    for(int i=0 ; i<vectorToBeAveraged.size() ; i++){
+        sum += vectorToBeAveraged[i];
+    }
+    return sum / vectorToBeAveraged.size();
+}
+
 int main(){
     vector<int> vectorToBeSummed{2, 6, 17, 7, 8, 21, 3, 9, 1, 13};
     cout<<"Sum of Vector Elements = "<<getSum(vectorToBeSummed)<<'\n';
+    cout<<"Average of Vector Elements = "<<getAverage(vectorToBeSummed)<<'\n';
 
     return 0;
 }
